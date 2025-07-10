85baab1c4498808ff8543c422d5298c56568afa1
Cesar Balzer <cesar.edenir@mkbank.com.br>
2025-07-10 14:56:08 -0300

    fix: ¬¬

--- src/config/telegram-logger.php
+++ src/config/telegram-logger.php
@@ -3,5 +3,5 @@
 return [
     'bot_token' => env('TELEGRAM_BOT_TOKEN'),
     'chat_id' => env('TELEGRAM_CHAT_ID'),
-    'level' => env('TELEGRAM_LOG_LEVEL')
+    'level' => env('LOG_TELEGRAM_LEVEL')
 ];
